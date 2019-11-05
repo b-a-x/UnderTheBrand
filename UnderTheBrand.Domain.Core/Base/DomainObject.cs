7ff8fa5745c6a9a1f47943cfbfc604fae0785584
i.odinokov <i.odinokov@axelot.ru>
2019-11-05 13:51:35 +0300

    Добавил проект

---
+++ UnderTheBrand.Domain.Core/Base/DomainObject.cs
@@ -0,0 +1,15 @@
+﻿using UnderTheBrand.Domain.Interfaces.Base;
+
+namespace UnderTheBrand.Domain.Core.Base
+{
+    /// <summary>
+    /// Базовый класс
+    /// </summary>
+    public class DomainObject : IDomainObject
+    {
+        /// <summary>
+        /// Индификатор
+        /// </summary>
+        public string Id { get; set; }
+    }
+}
---
+++ UnderTheBrand.Domain.Interfaces/Base/IDomainObject.cs
@@ -0,0 +1,10 @@
+﻿namespace UnderTheBrand.Domain.Interfaces.Base
+{
+    public interface IDomainObject
+    {
+        /// <summary>
+        /// Индификатор
+        /// </summary>
+        string Id { get; set; }
+    }
+}
