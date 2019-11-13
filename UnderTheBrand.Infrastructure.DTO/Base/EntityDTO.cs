b172a11b13bc2f5e512be62962a0b8a96a4402f0
i.odinokov <i.odinokov@axelot.ru>
2019-11-13 14:05:44 +0300

    Добавил DTO

---
+++ UnderTheBrand.Infrastructure.DTO/Base/EntityDTO.cs
@@ -0,0 +1,9 @@
+﻿using System;
+
+namespace UnderTheBrand.Infrastructure.DTO.Base
+{
+    public class EntityDTO
+    {
+        public Guid Id { get; set; }
+    }
+}
---
+++ UnderTheBrand.Infrastructure.DTO/Entities/PersonDTO.cs
@@ -0,0 +1,8 @@
+﻿using UnderTheBrand.Infrastructure.DTO.Base;
+
+namespace UnderTheBrand.Infrastructure.DTO.Entities
+{
+    public class PersonDTO : EntityDTO
+    {
+    }
+}
