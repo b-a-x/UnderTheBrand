cdd27eae44c9d24d0c7e9a5c7634193a1e1c43d2
i.odinokov <i.odinokov@axelot.ru>
2019-11-14 12:35:19 +0300

    Добавил карту с ошибками

---
+++ UnderTheBrand.Domain.Core/Helpers/Errors.cs
@@ -0,0 +1,18 @@
+﻿using UnderTheBrand.Domain.Core.Values;
+
+namespace UnderTheBrand.Domain.Core.Helpers
+{
+    public static class Errors
+    {
+        public static class Person
+        {
+            public static Error NameIsTaken(string name) => 
+                new Error("person.name.is.taken", $"Student name '{name}' is taken");
+        }
+        public static class General
+        {
+            public static Error NotFound(string entityName, long id) =>
+                new Error("record.not.found", $"'{entityName}' not found for Id '{id}'");
+        }
+    }
+}
--- UnderTheBrand.Domain.Core/Values/Age.cs
+++ UnderTheBrand.Domain.Core/Values/Age.cs
@@ -1,27 +1,27 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 using UnderTheBrand.Domain.Core.Base;
-using UnderTheBrand.Domain.Core.Interfaces.Base;
 
 namespace UnderTheBrand.Domain.Core.Values
 {
-    public class Age : ValueObject, IValueObjectValidation<int>
+    public class Age : ValueObject
     {
         private const string ErrorValidate = nameof(Age) + Validate;
 
         protected Age() { }
 
-        public Age(int value)
+        private Age(int value)
         {
-            if (!IsValid(value)) throw new ArgumentException(ErrorValidate);
             Value = value;
         }
 
         public int Value { get; }
 
-        public bool IsValid(int value)
+        public static Result<Age> Create(int input)
         {
-            return 10 <= value && value <= 120;
+            if (10 <= input && input <= 120)
+                return Result.Fail<Age>("Age is invalid");
+
+            return Result.Ok(new Age(input));
         }
 
         protected override IEnumerable<object> GetEqualityComponents()
