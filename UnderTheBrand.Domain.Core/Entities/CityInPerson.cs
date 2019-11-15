f6391db3c9f9a15b71a1852341ecccfbafdcbc56
i.odinokov <i.odinokov@axelot.ru>
2019-11-15 14:58:00 +0300

    Добавил связь один ко многим

---
+++ UnderTheBrand.Domain.Core/Entities/CityInPerson.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using PommaLabs.Thrower;
+using UnderTheBrand.Domain.Core.Base;
+using UnderTheBrand.Domain.Core.Values;
+
+namespace UnderTheBrand.Domain.Core.Entities
+{
+    public class CityInPerson : Entity
+    {
+        protected CityInPerson() { }
+
+        public CityInPerson(City city, Person person) : base(Guid.NewGuid())
+        {
+            Raise.ArgumentNullException.IfIsNull(city, nameof(city));
+            Raise.ArgumentNullException.IfIsNull(person, nameof(person));
+
+            City = city;
+            Person = person;
+        }
+
+        public City City { get; protected set; }
+
+        public Person Person { get; protected set; }
+    }
+}
--- UnderTheBrand.Domain.Core/Entities/Person.cs
+++ UnderTheBrand.Domain.Core/Entities/Person.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
 using PommaLabs.Thrower;
 using UnderTheBrand.Domain.Core.Base;
 using UnderTheBrand.Domain.Core.Values;
@@ -9,7 +11,7 @@ namespace UnderTheBrand.Domain.Core.Entities
     {
         protected Person() { }
 
-        public Person(PersonalName personalName, Age age): base(Guid.NewGuid())
+        public Person(PersonalName personalName, Age age) : base(Guid.NewGuid())
         {
             Raise.ArgumentNullException.IfIsNull(personalName, nameof(personalName));
             Raise.ArgumentNullException.IfIsNull(age, nameof(age));
@@ -21,5 +23,17 @@ namespace UnderTheBrand.Domain.Core.Entities
         public PersonalName PersonalName { get; set; }
 
         public Age Age { get; set; }
+
+        private List<CityInPerson> _cities;
+
+        public IReadOnlyCollection<City> Cities =>
+            _cities
+                .Select(x => x.City)
+                .ToList();
+
+        public void AddCity(City city)
+        {
+            _cities.Add(new CityInPerson(city, this));
+        }
     }
 }
