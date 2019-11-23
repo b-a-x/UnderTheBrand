9012f23f65ec5a83cd35d041c1953952533bd738
b_a_x <ily-odinokov@yandex.ru>
2019-11-23 15:59:29 +0300

    Добавил Xamarins.Form

---
+++ UnderTheBrand.Presentation/UnderTheBrand.Presentation/ViewModels/ItemDetailViewModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+using UnderTheBrand.Presentation.Models;
+
+namespace UnderTheBrand.Presentation.ViewModels
+{
+    public class ItemDetailViewModel : BaseViewModel
+    {
+        public Item Item { get; set; }
+        public ItemDetailViewModel(Item item = null)
+        {
+            Title = item?.Text;
+            Item = item;
+        }
+    }
+}
