0d38c89528a918fdf272a202cc4ea2ca7222ed2d
i.odinokov <i.odinokov@axelot.ru>
2019-11-28 11:16:31 +0300

    Добавил фактори

---
+++ UnderTheBrand.Domain.Core/Factory/Creator.cs
@@ -0,0 +1,9 @@
+﻿using UnderTheBrand.Domain.Core.Base;
+
+namespace UnderTheBrand.Domain.Core.Factory
+{
+    public abstract class Creator
+    {
+        public abstract EntityObject Create();
+    }
+}
