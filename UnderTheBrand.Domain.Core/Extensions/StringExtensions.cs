bf3afd2e4ce26590b58c536405808d9134be3124
i.odinokov <i.odinokov@axelot.ru>
2020-01-30 15:02:20 +0300

    Добавил информацию о типе

---
+++ UnderTheBrand.Domain.Core/Extensions/StringExtensions.cs
@@ -0,0 +1,9 @@
+﻿using System.Collections.Generic;
+
+namespace UnderTheBrand.Domain.Core.Extensions
+{
+    public static class StringExtensions
+    {
+        public static string Join(this IEnumerable<string> source, string separator) => string.Join(separator, source);
+    }
+}
--- UnderTheBrand.Presentation.Server/Controllers/TestController.cs
+++ UnderTheBrand.Presentation.Server/Controllers/TestController.cs
@@ -1,5 +1,6 @@
 ﻿using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using UnderTheBrand.Domain.Core.Infrastructure;
 using UnderTheBrand.Domain.Entity.Entities;
 using UnderTheBrand.Domain.Interfaces.Repositories;
 using UnderTheBrand.Domain.ValueObject.Values;
@@ -17,14 +18,14 @@ namespace UnderTheBrand.Presentation.Server.Controllers
             _repository = repository;
         }
 
-        [HttpGet(nameof(Get))]
+        [HttpGet("Get")]
         public async Task<IActionResult> Get()
         {
             //TODO: ответ мапить в dto
             return Ok(await _repository.GetAllAsync());
         }
 
-        [HttpPost(nameof(UpdatePerson))]
+        [HttpPost("UpdatePerson")]
         public async Task<IActionResult> UpdatePerson([FromBody] PersonViewModel vm)
         {
             Result<Name> firstName = Name.Create(vm.FirstName);
@@ -35,6 +36,23 @@ namespace UnderTheBrand.Presentation.Server.Controllers
             person = await _repository.AddAsync(person);
             _repository.SaveChanges();
             //TODO: ответ мапить в dto
+            return Ok(person);
+        }
+
+        [HttpGet("FastTypeInfo")]
+        public IActionResult FastTypeInfo()
+        {
+            Result<Name> firstName = Name.Create("Ilia");
+            Result<Name> lastName = Name.Create("Ilia");
+            Result<Age> age = Age.Create(25);
+            PersonalName personalName = new PersonalName(firstName.Value, lastName.Value);
+            var att = FastTypeInfo<Person>.Attributes;
+            var metoInfos = FastTypeInfo<Person>.PublicMethods;
+            var prop = FastTypeInfo<Person>.PublicProperties;
+            Person person = FastTypeInfo<Person>.Create(personalName, age.Value);
+            var method = FastTypeInfo<Person>.PropertyGetter<Person, PersonalName>("PersonalName");
+            var method2 = FastTypeInfo<Person>.PropertySetter<Person, PersonalName>("PersonalName");
+
             return Ok(person);
         }
     }
