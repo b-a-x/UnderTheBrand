01a80b6114f78c5fbc71a86146d6f935a308b3a7
i.odinokov <i.odinokov@axelot.ru>
2020-01-30 15:47:01 +0300

    Тест

---
+++ UnderTheBrand.Domain.Core.Tests/Infrastructure/FastTypeInfoTests.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using UnderTheBrand.Domain.Core.Base;
+using UnderTheBrand.Domain.Core.Infrastructure;
+
+namespace UnderTheBrand.Domain.Core.Tests.Infrastructure
+{
+    [TestClass]
+    public class FastTypeInfoTests
+    {
+        [TestMethod]
+        public void Properties_MoreZero_IsTrue()
+        {
+            // arrange
+            var props = FastTypeInfo<HasIdBaseTest>.PublicProperties;
+            // act
+
+            // assert
+            Assert.IsTrue(props.Length > 0);
+        }
+
+        [TestMethod]
+        public void Properties_One_IsTrue()
+        {
+            // arrange
+            var props = FastTypeInfo<HasIdBaseTest>.PublicProperties;
+            // act
+
+            // assert
+            Assert.IsTrue(props.Length.Equals(1));
+        }
+
+        private class HasIdBaseTest : HasIdBase<string>
+        {
+        }
+    }
+}
