f71e8b7d0fbc95206b5a431ebcf7999439ad75dc
b_a_x <ily-odinokov@yandex.ru>
2020-04-11 22:35:24 +0300

    Добавил конфигурация web host

--- Presentation.Server/Program.cs
+++ Presentation.Server/Program.cs
@@ -1,4 +1,5 @@
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
 
@@ -6,20 +7,30 @@ namespace UnderTheBrand.Presentation.Server
 {
     public class Program
     {
+        private const string AppSettingsJson = "appsettings.json";
+
         public static void Main(string[] args)
         {
             CreateHostBuilder(args).Build().Run();
         }
 
-        internal static IHostBuilder CreateHostBuilder(string[] args) =>
-            Host.CreateDefaultBuilder(args)
-                .ConfigureLogging((context, logging) =>
-                {
-                    logging.AddFile();
-                })
+        private static IHostBuilder CreateHostBuilder(string[] args)
+        {
+            return Host.CreateDefaultBuilder(args)
+                .ConfigureLogging((context, logging) => { logging.AddFile(); })
                 .ConfigureWebHostDefaults(webBuilder =>
                 {
+                    webBuilder.UseUrls(GetUrls());
                     webBuilder.UseStartup<Startup>();
                 });
+        }
+
+        private static string GetUrls()
+        {
+            IConfigurationRoot configuration = new ConfigurationBuilder()
+                .AddJsonFile(AppSettingsJson, false, true)
+                .Build();
+            return configuration.GetValue<string>("launchSettings:componentBaseUrl");
+        }
     }
 }
